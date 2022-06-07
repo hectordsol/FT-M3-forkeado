fe3f9500b5de147c77f17b2a4639c3b3abeef2e2
hectordsol <hectordsol@gmail.com>
2022-06-07 09:15:11 -0300

    Web Server

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,14 @@
-var fs  = require("fs")
-var http  = require("http")
+var http = require('http');
+var fs   = require('fs');
 
-// Escribí acá tu servidor
+http.createServer( function(req, res){ 
+    fs.readFile(`${__dirname}/images/${req.url}.jpg`, function(err, data){
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        } else {
+        res.writeHead(200, {'Content-Type': 'image/jpg'});
+        res.end(data);
+        }
+    });
+}).listen(1337, '127.0.0.1');	
